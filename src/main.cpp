4a6c2b7ba4c21012a6ee88d2c53ffc49de748cc2
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-17 13:52:07 +0545

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
-#include "../include/dotenv.hpp"
+#include "dotenv.hpp"
 #include <iostream>
 
 int main (int argc, char *argv[]) {
 	dotenv::init();
 	std::cout << dotenv::getenv("API_KEY") << "\n";
-
 	return 0;
 }
